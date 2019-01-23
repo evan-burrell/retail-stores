d96bff5719ea0391af8f172ab16563293123c7be
Evan Burrell <evan.burrell@ampersandit.co.uk>
2019-01-23 15:44:35 +0000

    Apply fixes from StyleCI

--- Ui/Component/Listing/Column/StoreActions.php
+++ Ui/Component/Listing/Column/StoreActions.php
@@ -3,8 +3,8 @@
 namespace Ampersand\Stores\Ui\Component\Listing\Column;
 
 use Magento\Framework\UrlInterface;
-use Magento\Framework\View\Element\UiComponentFactory;
 use Magento\Framework\View\Element\UiComponent\ContextInterface;
+use Magento\Framework\View\Element\UiComponentFactory;
 
 class StoreActions extends \Magento\Ui\Component\Listing\Columns\Column
 {
